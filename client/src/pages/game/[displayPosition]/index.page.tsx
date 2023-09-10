e9d83aecfc51cfc8e020c0046341cbe7fe141e66
yu7400ki <yu7400ki@gmail.com>
2023-09-11 03:39:01 +0900

    Page

--- client/src/pages/game/[displayPosition]/index.page.tsx
+++ client/src/pages/game/[displayPosition]/index.page.tsx
@@ -1,24 +1,10 @@
-import { ENEMY_HALF_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH } from 'commonConstantsWithClient';
-import type { BulletModel, EnemyModel, PlayerModel } from 'commonTypesWithClient/models';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { Image, Layer, Stage } from 'react-konva';
-import Boom from 'src/components/Effect/Boom';
-import { Bullet } from 'src/components/Entity/Bullet';
-import { Enemy } from 'src/components/Entity/Enemy';
-import { Player } from 'src/components/Entity/Player';
-import { staticPath } from 'src/utils/$path';
+import { useEffect } from 'react';
 import { apiClient } from 'src/utils/apiClient';
-import { computePosition } from 'src/utils/computePosition';
-import useImage from 'use-image';
+import { Game } from './@components/Game';
 import styles from './index.module.css';
 
-type WindowSize = {
-  width: number;
-  height: number;
-};
-
-const Game = () => {
+const Page = () => {
   const router = useRouter();
   let displayPosition: number | null = null;
   if (typeof router.query.displayPosition === 'string') {
@@ -27,78 +13,6 @@ const Game = () => {
       displayPosition = parsed;
     }
   }
-
-  const [players, setPlayers] = useState<PlayerModel[]>([]);
-  const [enemies, setEnemies] = useState<EnemyModel[]>([]);
-  const [bullets, setBullets] = useState<BulletModel[]>([]);
-  //TODO: もし、これ以外のエフェクトを追加する場合は、それぞれのエフェクトを区別する型を作成する
-  const [effectPosition, setEffectPosition] = useState<number[][]>([]);
-  const [windowSize, setWindowSize] = useState<WindowSize>({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  const [backgroundImage] = useImage(staticPath.images.odaiba_jpg);
-
-  const fetchPlayers = async () => {
-    const res = await apiClient.player.$get({
-      query: { displayNumber: Number(displayPosition) },
-    });
-    setPlayers(res);
-  };
-
-  const fetchEnemies = async () => {
-    const res = await apiClient.enemy.$get();
-    const killedEnemies = enemies.filter((enemy) => !res.some((e) => e.id === enemy.id));
-    if (killedEnemies.length > 0) {
-      killedEnemies.forEach((enemy) => {
-        const pos = computePosition(enemy.createdPos, enemy.createdAt, enemy.direction);
-        setEffectPosition((prev) => [
-          ...prev,
-          [pos.x - ENEMY_HALF_WIDTH, pos.y - ENEMY_HALF_WIDTH],
-        ]);
-      });
-    }
-    setEnemies(res);
-  };
-
-  const fetchBullets = async () => {
-    const res = await apiClient.bullet.$get({
-      query: { displayNumber: Number(displayPosition) },
-    });
-    if (res.length > bullets.length) {
-      const audio = new Audio(staticPath.sounds.shot_mp3);
-      audio.play();
-    }
-    setBullets(res);
-  };
-
-  useEffect(() => {
-    const cancelId = requestAnimationFrame(async () => {
-      await Promise.all([fetchPlayers(), fetchEnemies(), fetchBullets()]);
-    });
-    return () => cancelAnimationFrame(cancelId);
-  });
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setEffectPosition((prev) => prev.slice(1));
-    }, 1000);
-
-    return () => clearTimeout(timeoutId);
-  }, [effectPosition]);
-
-  useEffect(() => {
-    const set = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener('resize', set);
-    return () => window.removeEventListener('resize', set);
-  }, []);
 
   useEffect(() => {
     const redirectToLobby = async () => {
@@ -110,52 +24,15 @@ const Game = () => {
     redirectToLobby();
   }, [router, displayPosition]);
 
+  if (displayPosition === null) {
+    return null;
+  }
+
   return (
     <div className={styles.canvasContainer}>
-      <Stage
-        width={SCREEN_WIDTH}
-        height={SCREEN_HEIGHT}
-        style={{
-          transform: `
-              scale(${windowSize.width / SCREEN_WIDTH}, ${windowSize.height / SCREEN_HEIGHT})
-              translateX(${(windowSize.width - SCREEN_WIDTH) / 2}px)
-              translateY(${(windowSize.height - SCREEN_HEIGHT) / 2}px)
-              `,
-        }}
-      >
-        <Layer>
-          <Image
-            image={backgroundImage}
-            width={SCREEN_WIDTH}
-            height={SCREEN_HEIGHT}
-            x={0}
-            y={0}
-            opacity={0.8}
-          />
-        </Layer>
-        <Layer>
-          {bullets.map((bullet) => (
-            <Bullet displayPosition={displayPosition ?? 0} bullet={bullet} key={bullet.id} />
-          ))}
-        </Layer>
-        <Layer>
-          {players.map((player) => (
-            <Player displayPosition={displayPosition ?? 0} player={player} key={player.id} />
-          ))}
-        </Layer>
-        <Layer>
-          {enemies.map((enemy) => (
-            <Enemy displayPosition={displayPosition ?? 0} enemy={enemy} key={enemy.id} />
-          ))}
-        </Layer>
-        <Layer>
-          {effectPosition.map((position, index) => (
-            <Boom displayPosition={displayPosition ?? 0} position={position} key={index} />
-          ))}
-        </Layer>
-      </Stage>
+      <Game displayPosition={displayPosition} />
     </div>
   );
 };
 
-export default Game;
+export default Page;
